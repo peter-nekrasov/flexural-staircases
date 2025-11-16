cab9ad7a4c8fcdb7fdc5221b1e874971d34f0cba
Tristan Goodwill <48371600+tristangdwl@users.noreply.github.com>
2025-11-16 13:06:33 -0600

    try flat boundary

--- free_plate.m
+++ free_plate.m
@@ -13,7 +13,7 @@ targ = []; targ.r = [X(:).'; Y(:).'];
 
 if true
     cparams = []; cparams.ta = -d/2; cparams.tb = d/2;
-    nch = 20; A = 1;
+    nch = 20; A = 0;
     chnkr = chunkerfuncuni(@(t) cos_func(t,d,A),nch,cparams);
     chnkr = reverse(chnkr);
     wtarg = cos_func(targ.r(1,:),d,A) ;
@@ -21,10 +21,11 @@ if true
     src = []; src.r = [0;-2]; src.n = [1;0];
     % src = []; src.r = [0;2]; src.n = [1;0];
 else
-    chnkr = chunkerfunc(@starfish,struct('eps',1e-10)); chnkr = 0.25*chnkr;
+    chnkr = chunkerfunc(@(t) starfish(t,3,0.1),struct('eps',1e-10,'maxchunklen',.1)); chnkr = 0.25*chnkr;
+    chnkr = chnkr*1.3;
     targmod = real([mod(targ.r(1,:)+d/2,d)-d/2;targ.r(2,:)]);
     iout = ~chunkerinterior(chnkr,targmod);
-    src = []; src.r = [0;0]; src.n = [1;0];
+    src = []; src.r = [-0.01;-0.01]; src.n = [1;0];
 end
 
 targout = []; targout.r = targ.r(:,iout);
@@ -46,10 +47,27 @@ ht = 1.02*d; hb = -1.02*d;
 %%
 
 
-ising = 1;
+ising = 0;
 fkern1 =  @(s,t) chnk.flex2dquas.kern(zk, s, t, 'free_plate',kappa,d,sn,pxys_l,cs_l,l,ising,nu);
 double = @(s,t) chnk.lap2dquas.kern(s,t,'d',kappa,d,pxys_l,cs_l,l,ising);
 hilbert = @(s,t) chnk.lap2dquas.kern(s,t,'hilb',kappa,d,pxys_l,cs_l,l,ising);
+opts = [];
+opts.sing = 'smooth';
+
+opts2 = [];
+opts2.sing = 'smooth';
+
+% building system matrix
+
+start = tic;
+sysmat1 = chunkermat(chnkr,fkern1, opts);
+D = chunkermat(chnkr, double, opts);
+H = chunkermat(chnkr, hilbert, opts2);     
+
+
+fkern1 =  @(s,t) chnk.flex2d.kern(zk, s, t, 'free_plate',nu);
+double = @(s,t) chnk.lap2d.kern(s,t,'d');
+hilbert = @(s,t) chnk.lap2d.kern(s,t,'hilb');
 
 opts = [];
 opts.sing = 'log';
@@ -59,10 +77,11 @@ opts2.sing = 'pv';
 
 % building system matrix
 
-start = tic;
-sysmat1 = chunkermat(chnkr,fkern1, opts);
-D = chunkermat(chnkr, double, opts);
-H = chunkermat(chnkr, hilbert, opts2);     
+sysmat1_0 = chunkermat(chnkr,fkern1, opts);
+D_0 = chunkermat(chnkr, double, opts);
+H_0 = chunkermat(chnkr, hilbert, opts2); 
+
+sysmat1 = sysmat1 + sysmat1_0; D = D + D_0; H = H + H_0;
 
 sysmat = zeros(2*chnkr.npt);
 sysmat(1:2:end,1:2:end) = sysmat1(1:4:end,1:2:end) + sysmat1(3:4:end,1:2:end)*H  - 2*((1+nu)/2)^2*D*D;
@@ -80,8 +99,8 @@ fprintf('%5.2e s : time to assemble matrix\n',t1)
 
 %%
 
-skern =  @(s,t) chnk.flex2dquas.kern(zk, s, t, 's',kappa,d,sn,pxys_l,cs_l,l,ising);
-bskern =  @(s,t) chnk.flex2dquas.kern(zk, s, t, 'free_plate_bcs',kappa,d,sn,pxys_l,cs_l,l,ising,nu);
+skern =  @(s,t) chnk.flex2dquas.kern(zk, s, t, 's',kappa,d,sn,pxys_l,cs_l,l,1);
+bskern =  @(s,t) chnk.flex2dquas.kern(zk, s, t, 'free_plate_bcs',kappa,d,sn,pxys_l,cs_l,l,1,nu);
 
 rhs = -bskern(src,chnkr);
 
@@ -125,7 +144,8 @@ quiver(chnkr)
 hold on
 plot(chnkrs,'k.')
 scatter(src.r(1,:),src.r(2,:))
-h = pcolor(X,Y, reshape(log10(abs(us)),size(X))); h.EdgeColor = 'None';
+% h = pcolor(X,Y, reshape(log10(abs(us)),size(X))); h.EdgeColor = 'None';
+h = pcolor(X,Y, reshape((real(us)),size(X))); h.EdgeColor = 'None';
 colorbar
 hold off
 axis equal
