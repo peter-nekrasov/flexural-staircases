2355f4a7467537c8f52681b422608b5c38e70e07
peter-nekrasov <pn3@uchicago.edu>
2026-01-11 15:15:52 -0600

    added fifth derivatives

--- test_green_derivatives.m
+++ test_green_derivatives.m
@@ -23,33 +23,60 @@ d1 = [-1/2 0 1/2]/h;
 
 src = []; src.r = [0.1;0.2]; 
 targ = []; targ.r = [0.4;0.1] + h*[-1:1;0*(-1:1)];
-[val,grad,hess,third,fourth] = chnk.flex2dquas.green(src.r,targ.r,zk,xi,d,sn,l,ising);
+[val,grad,hess,third,fourth,fifth] = chnk.flex2dquas.green(src.r,targ.r,zk,xi,d,sn,l,ising);
 
 err1 = abs(d1*val - grad(2,1,1))  / abs(grad(2,1,1)) % G_{x}
 err2 = abs(d1*grad(:,:,1) - hess(2,1,1))  / abs(hess(2,1,1)) % G_{xx}
 err3 = abs(d1*hess(:,:,1) - third(2,1,1))  / abs(third(2,1,1)) % G_{xxx}
 err4 = abs(d1*third(:,:,1) - fourth(2,1,1))  / abs(fourth(2,1,1)) % G_{xxxx}
-
+err5 = abs(d1*fourth(:,:,1) - fifth(2,1,1))  / abs(fifth(2,1,1)) % G_{xxxx}
 
 targ = []; targ.r = [0.4;0.1] + h*[0*(-1:1);(-1:1)];
-[val,grad,hess,third,fourth] = chnk.flex2dquas.green(src.r,targ.r,zk,xi,d,sn,l,ising);
+[val,grad,hess,third,fourth,fifth] = chnk.flex2dquas.green(src.r,targ.r,zk,xi,d,sn,l,ising);
 
-err5 = abs(d1*val - grad(2,1,2))  / abs(grad(2,1,1)) % G_{y}
-err6 = abs(d1*grad(:,:,1) - hess(2,1,2))  / abs(hess(2,1,2)) % G_{xy}
-err7 = abs(d1*grad(:,:,2) - hess(2,1,3))  / abs(hess(2,1,3)) % G_{yy}
+err6 = abs(d1*val - grad(2,1,2))  / abs(grad(2,1,1)) % G_{y}
+err7 = abs(d1*grad(:,:,1) - hess(2,1,2))  / abs(hess(2,1,2)) % G_{xy}
+err8 = abs(d1*grad(:,:,2) - hess(2,1,3))  / abs(hess(2,1,3)) % G_{yy}
 
-err8 = abs(d1*hess(:,:,1) - third(2,1,2))  / abs(third(2,1,2)) % G_{xxy}
-err9 = abs(d1*hess(:,:,2) - third(2,1,3))  / abs(third(2,1,3)) % G_{xyy}
-err10 = abs(d1*hess(:,:,3) - third(2,1,4))  / abs(third(2,1,4)) % G_{yyy}
+err9 = abs(d1*hess(:,:,1) - third(2,1,2))  / abs(third(2,1,2)) % G_{xxy}
+err10 = abs(d1*hess(:,:,2) - third(2,1,3))  / abs(third(2,1,3)) % G_{xyy}
+err11 = abs(d1*hess(:,:,3) - third(2,1,4))  / abs(third(2,1,4)) % G_{yyy}
 
-err11 = abs(d1*third(:,:,1) - fourth(2,1,2))  / abs(fourth(2,1,2)) % G_{xxxy}
-err12 = abs(d1*third(:,:,2) - fourth(2,1,3))  / abs(fourth(2,1,3)) % G_{xxyy}
-err13 = abs(d1*third(:,:,3) - fourth(2,1,4))  / abs(fourth(2,1,4)) % G_{xyyy}
-err14 = abs(d1*third(:,:,4) - fourth(2,1,5))  / abs(fourth(2,1,5)) % G_{yyyy}
+err12 = abs(d1*third(:,:,1) - fourth(2,1,2))  / abs(fourth(2,1,2)) % G_{xxxy}
+err13 = abs(d1*third(:,:,2) - fourth(2,1,3))  / abs(fourth(2,1,3)) % G_{xxyy}
+err14 = abs(d1*third(:,:,3) - fourth(2,1,4))  / abs(fourth(2,1,4)) % G_{xyyy}
+err15 = abs(d1*third(:,:,4) - fourth(2,1,5))  / abs(fourth(2,1,5)) % G_{yyyy}
+
+err16 = abs(d1*fourth(:,:,1) - fifth(2,1,2))  / abs(fifth(2,1,2)) % G_{xxxxy}
+err17 = abs(d1*fourth(:,:,2) - fifth(2,1,3))  / abs(fifth(2,1,3)) % G_{xxxyy}
+err18 = abs(d1*fourth(:,:,3) - fifth(2,1,4))  / abs(fifth(2,1,4)) % G_{xxyyy}
+err19 = abs(d1*fourth(:,:,4) - fifth(2,1,5))  / abs(fifth(2,1,5)) % G_{xyyyy}
+err20 = abs(d1*fourth(:,:,5) - fifth(2,1,6))  / abs(fifth(2,1,5)) % G_{yyyyy}
 
 
+assert(err1 < 1e-8)
+assert(err2 < 1e-8)
+assert(err3 < 1e-8)
+assert(err4 < 1e-8)
+assert(err5 < 5e-6)
+assert(err6 < 1e-8)
+assert(err7 < 1e-8)
+assert(err8 < 1e-8)
+assert(err9 < 1e-8)
+assert(err10 < 1e-8)
+assert(err11 < 1e-8)
+assert(err12 < 1e-8)
+assert(err13 < 1e-8)
+assert(err14 < 1e-8)
+assert(err15 < 1e-8)
+assert(err16 < 5e-6)
+assert(err17 < 1e-6)
+assert(err18 < 1e-7)
+assert(err19 < 1e-7)
+assert(err20 < 1e-7)
 
 
+return
 
 %%
 src2 = [0;0];
