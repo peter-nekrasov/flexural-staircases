d1aebc868f568cfcf32625ae0ff48f061b268c89
peter-nekrasov <pn3@uchicago.edu>
2026-01-12 16:36:38 -0600

    added stable evaluator for sixth derivatives

--- test_green_derivatives.m
+++ test_green_derivatives.m
@@ -23,13 +23,14 @@ d1 = [-1/2 0 1/2]/h;
 
 src = []; src.r = [0.1;0.2]; 
 targ = []; targ.r = [0.4;0.1] + h*[-1:1;0*(-1:1)];
-[val,grad,hess,third,fourth,fifth] = chnk.flex2dquas.green(src.r,targ.r,zk,xi,d,sn,l,ising);
+[val,grad,hess,third,fourth,fifth,sixth] = chnk.flex2dquas.green(src.r,targ.r,zk,xi,d,sn,l,ising);
 
 err1 = abs(d1*val - grad(2,1,1))  / abs(grad(2,1,1)) % G_{x}
 err2 = abs(d1*grad(:,:,1) - hess(2,1,1))  / abs(hess(2,1,1)) % G_{xx}
 err3 = abs(d1*hess(:,:,1) - third(2,1,1))  / abs(third(2,1,1)) % G_{xxx}
 err4 = abs(d1*third(:,:,1) - fourth(2,1,1))  / abs(fourth(2,1,1)) % G_{xxxx}
 err5 = abs(d1*fourth(:,:,1) - fifth(2,1,1))  / abs(fifth(2,1,1)) % G_{xxxx}
+err6 = abs(d1*fifth(:,:,1) - sixth(2,1,1))  / abs(sixth(2,1,1)) % G_{xxxx}
 
 targ = []; targ.r = [0.4;0.1] + h*[0*(-1:1);(-1:1)];
 [val,grad,hess,third,fourth,fifth] = chnk.flex2dquas.green(src.r,targ.r,zk,xi,d,sn,l,ising);
@@ -109,3 +110,49 @@ norm(val2(:) -val_true(:))
 figure(1);clf
 h = pcolor(X,Y,reshape(real(val2(:,:,1)),size(X))); h.EdgeColor = 'None';
 
+%% derivatives of hkdiffgreen
+
+zk = 1.5;
+h = 0.00001;
+
+d1 = [-1/2 0 1/2]/h;
+
+src = []; src.r = [2.1;2.2]; 
+targ = []; targ.r = [0.4;0.1] + h*[-1:1;0*(-1:1)];
+[val,grad,hess,third,fourth,fifth,sixth] = chnk.flex2d.hkdiffgreen(zk,src.r,targ.r);
+
+err1 = abs(d1*val - grad(2,1,1))  / abs(grad(2,1,1)) % G_{x}
+err2 = abs(d1*grad(:,:,1) - hess(2,1,1))  / abs(hess(2,1,1)) % G_{xx}
+err3 = abs(d1*hess(:,:,1) - third(2,1,1))  / abs(third(2,1,1)) % G_{xxx}
+err4 = abs(d1*third(:,:,1) - fourth(2,1,1))  / abs(fourth(2,1,1)) % G_{xxxx}
+err5 = abs(d1*fourth(:,:,1) - fifth(2,1,1))  / abs(fifth(2,1,1)) % G_{xxxx}
+err6 = abs(d1*fifth(:,:,1) - sixth(2,1,1))  / abs(sixth(2,1,1)) % G_{xxxx}
+
+targ = []; targ.r = [0.4;0.1] + h*[0*(-1:1);(-1:1)];
+[val,grad,hess,third,fourth,fifth] = chnk.flex2d.hkdiffgreen(zk,src.r,targ.r);
+
+err6 = abs(d1*val - grad(2,1,2))  / abs(grad(2,1,1)) % G_{y}
+err7 = abs(d1*grad(:,:,1) - hess(2,1,2))  / abs(hess(2,1,2)) % G_{xy}
+err8 = abs(d1*grad(:,:,2) - hess(2,1,3))  / abs(hess(2,1,3)) % G_{yy}
+
+err9 = abs(d1*hess(:,:,1) - third(2,1,2))  / abs(third(2,1,2)) % G_{xxy}
+err10 = abs(d1*hess(:,:,2) - third(2,1,3))  / abs(third(2,1,3)) % G_{xyy}
+err11 = abs(d1*hess(:,:,3) - third(2,1,4))  / abs(third(2,1,4)) % G_{yyy}
+
+err12 = abs(d1*third(:,:,1) - fourth(2,1,2))  / abs(fourth(2,1,2)) % G_{xxxy}
+err13 = abs(d1*third(:,:,2) - fourth(2,1,3))  / abs(fourth(2,1,3)) % G_{xxyy}
+err14 = abs(d1*third(:,:,3) - fourth(2,1,4))  / abs(fourth(2,1,4)) % G_{xyyy}
+err15 = abs(d1*third(:,:,4) - fourth(2,1,5))  / abs(fourth(2,1,5)) % G_{yyyy}
+
+err16 = abs(d1*fourth(:,:,1) - fifth(2,1,2))  / abs(fifth(2,1,2)) % G_{xxxxy}
+err17 = abs(d1*fourth(:,:,2) - fifth(2,1,3))  / abs(fifth(2,1,3)) % G_{xxxyy}
+err18 = abs(d1*fourth(:,:,3) - fifth(2,1,4))  / abs(fifth(2,1,4)) % G_{xxyyy}
+err19 = abs(d1*fourth(:,:,4) - fifth(2,1,5))  / abs(fifth(2,1,5)) % G_{xyyyy}
+err20 = abs(d1*fourth(:,:,5) - fifth(2,1,6))  / abs(fifth(2,1,5)) % G_{yyyyy}
+
+err21 = abs(d1*fifth(:,:,1) - sixth(2,1,2))  / abs(sixth(2,1,2)) % G_{xxxxy}
+err22 = abs(d1*fifth(:,:,2) - sixth(2,1,3))  / abs(sixth(2,1,3)) % G_{xxxyy}
+err23 = abs(d1*fifth(:,:,3) - sixth(2,1,4))  / abs(sixth(2,1,4)) % G_{xxyyy}
+err24 = abs(d1*fifth(:,:,4) - sixth(2,1,5))  / abs(sixth(2,1,5)) % G_{xyyyy}
+err25 = abs(d1*fifth(:,:,5) - sixth(2,1,6))  / abs(sixth(2,1,6)) % G_{yyyyy}
+err26 = abs(d1*fifth(:,:,6) - sixth(2,1,7))  / abs(sixth(2,1,7)) % G_{yyyyy}
