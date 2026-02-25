c9148ec62ee1d8af3240c636facb003b1537a370
Tristan Goodwill <48371600+tristangdwl@users.noreply.github.com>
2026-02-25 11:39:15 -0600

    Update PoincareBertrand_test.m

--- PoincareBertrand_test.m
+++ PoincareBertrand_test.m
@@ -30,19 +30,69 @@ for i = 1:length(echnks)
 
 chnkr = echnks(i);
 
-ising = 1;
-double = kernel(@(s,t) chnk.lap2dquas.kern(s,t,'d',kappa,d,s0_l,sn_l,l,ising));
-double.sing = 'smooth';
-hilbert = kernel(@(s,t) chnk.lap2dquas.kern(s,t,'hilb',kappa,d,s0_l,sn_l,l,ising));
-hilbert.sing = 'pv';
+ising = 0;
+nsub = 1;
+double = @(s,t) chnk.lap2dquas.kern(s,t,'d',kappa,d,s0_l,sn_l,l,ising,nsub);
+hilbert = @(s,t) chnk.lap2dquas.kern(s,t,'hilb',kappa,d,s0_l,sn_l,l,ising,nsub);
+opts = [];
+opts.sing = 'smooth';
+opts.quad = 'native';
 
-dmat = chunkermat(chnkr,double);
-hmat = chunkermat(chnkr,hilbert);
+opts2 = [];
+opts2.sing = 'smooth';
+opts.quad = 'native';
+
+
+% building system matrix
+
+start = tic;
+D = chunkermat(chnkr, double, opts2);
+H = chunkermat(chnkr, hilbert, opts2);     
+
+D = reshape(D,nkappa,chnkr.npt,chnkr.npt);
+H = reshape(H,nkappa,chnkr.npt,chnkr.npt);
+
+
+fkern =  @(s,t) chnk.flex2d.kern(zk, s, t, 'free_plate',nu);
+double = @(s,t) chnk.lap2d.kern(s,t,'d');
+hilbert = @(s,t) chnk.lap2d.kern(s,t,'hilb');
+
+opts = [];
+opts.sing = 'log';
+
+opts2 = [];
+opts2.sing = 'pv';
+
+% building system matrix
+
+D_0 = chunkermat(chnkr, double, opts);
+H_0 = chunkermat(chnkr, hilbert, opts2); 
+
+D_0 = reshape(D_0,1,chnkr.npt,chnkr.npt);
+H_0 = reshape(H_0,1,chnkr.npt,chnkr.npt);
+
+alpha = reshape(exp(1i*kappa(:)*d),[nkappa,1,1]); 
+for ii = -nsub:nsub
+    if ii ~= 0 
+        Hsub = chunkerkernevalmat(chnkr + ii*[d;0],hilbert,chnkr);
+        Dsub = chunkerkernevalmat(chnkr + ii*[d;0],double,chnkr);
+  
+        Hsub = reshape(Hsub,[1,chnkr.npt,chnkr.npt]);
+        Dsub = reshape(Dsub,[1,chnkr.npt,chnkr.npt]);
+
+        H_0 = H_0 + alpha.^(ii).*Hsub;
+        D_0 = D_0 + alpha.^(ii).*Dsub;
+
+    end
+end
+
+dmat = D + D_0; hmat = H + H_0;
+
 
 rhs = chnk.lap2dquas.kern(src,chnkr,'s',kappa,d,s0_l,sn_l,l);
 for k = 1:nkappa
-    H = hmat(k:nkappa:end,:);
-    D = dmat(k:nkappa:end,:);
+    H = squeeze(hmat(k,:,:));
+    D = squeeze(dmat(k,:,:));
     R = rhs(k:nkappa:end);
 a = 0.25*H * (H * R);
 b = -0.25*R + D * (D * R);
